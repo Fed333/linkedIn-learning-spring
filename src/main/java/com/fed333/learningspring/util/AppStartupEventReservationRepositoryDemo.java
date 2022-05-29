e14d219035807d6b3c7fdfb16a32e14a46ca3e65
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-29 13:23:17 +0300

    added demo code to show whether ReservationRepository is up and running

---
+++ src/main/java/com/fed333/learningspring/util/AppStartupEventReservationRepositoryDemo.java
@@ -0,0 +1,26 @@
+package com.fed333.learningspring.util;
+
+import com.fed333.learningspring.data.repo.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class AppStartupEventReservationRepositoryDemo implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ReservationRepository reservationRepo;
+
+    @Autowired
+    public AppStartupEventReservationRepositoryDemo(ReservationRepository reservationRepo) {
+        this.reservationRepo = reservationRepo;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        LocalDate resDate = LocalDate.of(2022, 1, 1);
+        reservationRepo.findAllByResDate(resDate).forEach(System.out::println);
+    }
+}
