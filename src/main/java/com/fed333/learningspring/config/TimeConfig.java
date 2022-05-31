a8d3267682bbbb2b7f69bef9830e77eda5f87a62
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-31 13:04:32 +0300

    registered Time primary bean in config class

---
+++ src/main/java/com/fed333/learningspring/config/TimeConfig.java
@@ -0,0 +1,21 @@
+package com.fed333.learningspring.config;
+
+import com.fed333.learningspring.util.datetime.LocalTimeImpl;
+import com.fed333.learningspring.util.datetime.Time;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class TimeConfig {
+
+    @Bean
+    @Scope("prototype")
+    @Primary
+    public Time getTime(){
+        System.out.println("TimeConfig.getTime() invoked!");
+        return new LocalTimeImpl();
+    }
+
+}
